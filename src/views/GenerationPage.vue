bf4547d0c1ceccdc7a12edcb24a25163502ba268
Matthew Walcher <matt@mwalcher.com>
2025-01-30 12:54:49 +0000

    add gen name as title and redirect when no gen is found

--- src/views/GenerationPage.vue
+++ src/views/GenerationPage.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
+import { useChangeCase } from '@vueuse/integrations/useChangeCase';
 import { storeToRefs } from 'pinia';
 import { onMounted, ref } from 'vue';
-import { RouterLink, useRoute } from 'vue-router';
+import { RouterLink, useRoute, useRouter } from 'vue-router';
 
 import { usePokeApiStore } from '@/stores/pokeApi';
 import type { IsMergedGenerationData } from '@/types/generation';
 
 const route = useRoute();
+const router = useRouter();
 const pokeApiStore = usePokeApiStore();
 const { generations } = storeToRefs(pokeApiStore);
 
@@ -15,14 +17,19 @@ const generation = ref<IsMergedGenerationData>();
 onMounted(async () => {
   const generationId = Number(route.params.id);
   generation.value = generations.value.find((gen) => gen.id === generationId);
+  if (!generation.value) {
+    router.push('/');
+  }
 });
 </script>
 
 <template>
-  <h1>Generation</h1>
-  <RouterLink to="/">All Generations</RouterLink>
+  <template v-if="generation">
+    <h1>{{ useChangeCase(generation.main_region.name, 'capitalCase') }}</h1>
+    <RouterLink to="/">All Generations</RouterLink>
 
-  <pre>
-    {{ generation }}
-  </pre>
+    <pre>
+    {{ Object.keys(generation) }}
+    </pre>
+  </template>
 </template>
