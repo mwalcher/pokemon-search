d5044f27d5a9ae986ec64fe1d55dd64d2eb4f092
Matthew Walcher <matt@mwalcher.com>
2025-04-26 20:37:59 +0100

    add generation back link

--- src/views/VersionPage.vue
+++ src/views/VersionPage.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 import { usePokeApiStore } from '@/stores/pokeApi';
+import type { IsMergedGenerationData } from '@/types/generation';
 import type { IsMergedVersionData, IsVersionStorageData } from '@/types/versions';
 import { getImageUrl, getVersionPath } from '@/utilities/image';
 import { getNameByLanguage } from '@/utilities/pokeApi';
@@ -13,9 +14,10 @@ const baseUrl = import.meta.url;
 const route = useRoute();
 const router = useRouter();
 const pokeApiStore = usePokeApiStore();
-const { versionsByGeneration } = storeToRefs(pokeApiStore);
+const { generations, versionsByGeneration } = storeToRefs(pokeApiStore);
 
 const versionId = Number(route.params.versionId);
+const generation = ref<IsMergedGenerationData>();
 const generationVersionData = ref<IsVersionStorageData>();
 const versionData = ref<IsMergedVersionData>();
 
@@ -35,12 +37,16 @@ onMounted(async () => {
   }
 
   versionData.value = generationVersionData.value.versions.find((version) => version.id === versionId);
+  generation.value = generations.value.find((gen) => gen.name === generationVersionData.value?.generation_name);
 });
 </script>
 
 <template>
-  <template v-if="versionData">
+  <template v-if="versionData && generation">
     <h1>{{ toCapitalCase(getNameByLanguage(versionData.names)) }}</h1>
+    <RouterLink :to="{ name: 'generation', params: { generationId: generation.id } }">
+      Back to {{ getNameByLanguage(generation.names) }}: {{ toCapitalCase(generation.main_region.name) }}
+    </RouterLink>
     <img
       :class="$style['cover-art']"
       :src="getImageUrl(baseUrl, getVersionPath(versionData))"
